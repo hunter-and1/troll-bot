bbd9ea8d501f686369f64e570a297eb3d56d979f
hunter-and1 <abdellah.elkadiri.dev@gmail.com>
2018-03-23 23:59:36 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var listsv = [];
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content.startsWith('&addSV')) {
+        var messagesplit = message.content.split(" ");
+        listsv.push(messagesplit[0]);
+        return;
   	}
+    
+    if (message.content.startsWith('&getSV')) {
+        message.reply(listsv[0]);
+        return;
+  	}
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
