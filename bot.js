a7772bb486eb9508d4ca84914185f7a198ac9de2
hunter-and1 <abdellah.elkadiri.dev@gmail.com>
2018-03-24 00:11:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,10 @@ client.on('message', message => {
   	}
     
     if (message.content.startsWith('&getSV')) {
+        var textreplay = "";
+        for (var i = 1; i <= listsv.length; i++){
+            textreplay += i+' '+listsv[i];
+        }
         message.channel.send(listsv.toString());
   	}
     
