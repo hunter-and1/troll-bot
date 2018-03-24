0acca0a5c143133f1d38bb2095172045bf308e34
hunter-and1 <abdellah.elkadiri.dev@gmail.com>
2018-03-24 21:27:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,39 +3,36 @@ const client = new Discord.Client();
 var listsv = [];
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
 client.on('message', message => {
 
-     if (message.content.startsWith('?say')) {
-         message.channel.send(message.content.replace('?say',''), {
-             tts: true
-            });
-  	}
+  if (message.content.startsWith('?say')) {
+    message.channel.send(message.content.replace('?say',''), {tts: true});
+  }
     
-    if (message.content.startsWith('?clan')) {
-         message.channel.send("˜͙ᴹᴰ྅ཱི"+message.author.replace('@',''));
-  	}
+  if (message.content.startsWith('?clan')) {
+    message.channel.send(message.author.replace('@',"˜͙ᴹᴰ྅ཱི"));
+  }
     
-    if (message.content.startsWith('?add')) {
-        var messagesplit = message.content.split(" ");
-        listsv.push(messagesplit[1]);
+  if (message.content.startsWith('?add')) {
+    var messagesplit = message.content.split(" ");
+    listsv.push(messagesplit[1]);
+	}
+    
+  if (message.content.startsWith('?sv')) {
+    var textreplay = "";
+    for (var i = 1; i <= listsv.length; i++)
+      textreplay += i+' - '+listsv[i-1]+'\n';
+    
+    message.channel.send(textreplay);
+  }
+    
+  if (message.content.startsWith("?clear")) {
+    listsv = [];
+  }
 
-  	}
-    
-    if (message.content.startsWith('?sv')) {
-        var textreplay = "";
-        for (var i = 1; i <= listsv.length; i++){
-            textreplay += i+' - '+listsv[i-1]+'\n';
-        }
-        message.channel.send(textreplay);
-        
-  	}
-    
-   if (message.content.startsWith("?clear")) {
-       listsv = [];
-     }
 });
 
 // THIS  MUST  BE  THIS  WAY
