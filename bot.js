6e25d8f73c55bbbb18e5428c155b58c1fc6206f6
hunter-and1 <abdellah.elkadiri.dev@gmail.com>
2018-03-24 00:43:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,10 +10,7 @@ client.on('message', message => {
     if (message.content.startsWith('?add')) {
         var messagesplit = message.content.split(" ");
         listsv.push(messagesplit[1]);
-        message.channel.send("", {embed: {
-            title: "this sv is added",
-            color: 0x06DF00
-        });
+
   	}
     
     if (message.content.startsWith('?sv')) {
@@ -27,21 +24,6 @@ client.on('message', message => {
     
    if (message.content.startsWith("?clear")) {
        listsv = [];
-       message.channel.send("", {embed: {
-        title: "not have any sv",
-        color: 0x00AE86
-      });
-       /*
-    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم مسح الشات",
-        color: 0x06DF00,
-        description: "تم مسح الرسائل "
-      }}).then(msg => {msg.delete(3000)});*/
      }
 });
 
