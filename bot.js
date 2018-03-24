065933b2720cd6aaf44fa9e5258bd779ff672cdb
hunter-and1 <abdellah.elkadiri.dev@gmail.com>
2018-03-24 00:33:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,18 @@ client.on('message', message => {
         message.channel.send(textreplay);
   	}
     
+   if (message.content.startsWith("?clear")) {
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل "
+      }}).then(msg => {msg.delete(3000)});
+     }
 });
 
 // THIS  MUST  BE  THIS  WAY
