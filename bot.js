ae1468249458301ac6b6641c718aa932c236ae00
Abdellah Elkadiri | GM <abdellah.elkadiri.dev@gmail.com>
2019-01-07 22:56:53 +0100

    no message

--- bot.js
+++ bot.js
@@ -113,13 +113,10 @@ client.on('message', message => {
   }
 
   if (message.content.startsWith("hmadi")) {
-
     var VC = message.member.voiceChannel;
-    //var VC = message.member.voice.channel;
     if (VC)
     {
       VC.join().then(connection => {
-          //const dispatcher = connection.playFile('./audio/hmadi.mp3');
           const dispatcher = connection.play('./audio/hmadi.mp3');
           dispatcher.on("end", end => {
             //setTimeout(function(){ 
